ee44820a64612c646495bd3b2071a82cab58e01f
hades89110 <s89110g@gmail.com>
2025-02-21 23:56:00 +0800

    加入專案檔案。

---
+++ projecth/Controllers/ItemsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using projecth.Models;
+
+namespace projecth.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var item = new Item() { Name="keyboard" };
+            return View(item);
+        }
+
+        public IActionResult Edit(int itemId)
+        {
+            return Content("id= " + itemId);
+        }
+    }
+}
